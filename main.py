b43d382ac5c92f8a7a7a2dd80e18a727fcc6642e
xs0203401 <yaohuang.liu@utexas.edu>
2018-08-06 16:10:27 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,153 @@
+# [START imports]
+import os
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
+
+import jinja2
+import webapp2
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)+"/templates"),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+# [END imports]
+
+DEFAULT_THEME = "default"
+
+def theme_key(theme_name=DEFAULT_THEME):
+    return ndb.Key('Theme', theme_name)
+
+class Author(ndb.Model):
+    identity = ndb.StringProperty(indexed=False)
+    email = ndb.StringProperty(indexed=False)
+
+class Report(ndb.Model):
+    author = ndb.StructuredProperty(Author)
+    theme = ndb.StringProperty(indexed=False)
+    title = ndb.StringProperty(indexed=False)
+    tag = ndb.StringProperty(indexed=False)
+    description = ndb.StringProperty(indexed=False)
+    image = ndb.BlobProperty(indexed=False)
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        
+        # guestbook_name = self.request.get('guestbook_name',
+        #                                   DEFAULT_GUESTBOOK_NAME)
+        # greetings_query = Greeting.query(
+        #     ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+        # greetings = greetings_query.fetch(10)
+        
+
+        user = users.get_current_user()
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+        
+        # Image Upload Url
+        # upload_url = blobsotre.create_upload_url('/create-report')
+
+        template_values = {
+            'user': user,
+            'url': url,
+            'url_linktext': url_linktext,
+            'action': upload_url,
+        }
+
+        
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+class CreateReport(webapp2.RequestHandler):
+
+    def post(self):
+        
+        report_theme = self.request.get('theme', DEFAULT_THEME)
+        report = Report(parent=theme_key(report_theme))
+
+        if users.get_current_user():
+            report.author = Author(
+                identity=users.get_current_user().user_id(),
+                email=users.get_current_user().email())
+        else:
+            report.author = Author(
+                identity="Anonymous",
+                email="unknown@unknown.com")
+
+        report.theme = self.request.get('theme')
+        report.title = self.request.get('title')
+        report.tag = self.request.get('tags')
+        report.description = self.request.get('description')
+
+        # Saving image
+        # upload = self.get_uploads()[0]
+        # report.image = upload.key()
+
+        report.put()
+
+        self.response.write('''<html><body>You wrote:''')
+        self.response.write(report.theme)
+        self.response.write(report.title)
+        self.response.write(report.tag)
+        self.response.write(report.description)
+        # self.response.write(blobstore.get(upload.key))
+        self.response.write('''<body><html>''')
+        self.response.write('success!')
+
+        # query_params = {'guestbook_name': guestbook_name}
+        # self.redirect('/?' + urllib.urlencode(query_params))
+
+
+class ThemesPage(webapp2.RequestHandler):
+
+    def get(self):
+        
+
+        template = JINJA_ENVIRONMENT.get_template('themes.html')
+        self.response.write(template.render(template_values))
+
+
+class Reports(webapp2.RequestHandler):
+
+    def get(self):
+
+        template = JINJA_ENVIRONMENT.get_template('reports.html')
+        self.response.write(template.render(template_values))
+
+
+class ManageThemes(webapp2.RequestHandler):
+
+    def get(self):
+
+
+        template = JINJA_ENVIRONMENT.get_template('manage.html')
+        self.response.write(template.render(template_values))
+
+
+
+
+# [START app]
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/themes', ThemesPage),
+    ('/book-reports', Reports),
+    ('/manage-themes', ManageThemes),
+    ('/create-report', CreateReport),
+s], debug=True)
+# [END app]
+
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,51 @@
+from google.appengine.api import users
+from google.appengine.ext import blobstore
+from google.appengine.ext import ndb
+from google.appengine.ext.webapp import blobstore_handlers
+import webapp2
+
+
+# This datastore model keeps track of which users uploaded which photos.
+class UserPhoto(ndb.Model):
+    user = ndb.StringProperty()
+    blob_key = ndb.BlobKeyProperty()
+
+
+class PhotoUploadFormHandler(webapp2.RequestHandler):
+    def get(self):
+        upload_url = blobstore.create_upload_url('/upload_photo')
+        # To upload files to the blobstore, the request method must be "POST"
+        # and enctype must be set to "multipart/form-data".
+        self.response.out.write("""
+<html><body>
+<form action="{0}" method="POST" enctype="multipart/form-data">
+  Upload File: <input type="file" name="file"><br>
+  <input type="submit" name="submit" value="Submit">
+</form>
+</body></html>""".format(upload_url))
+
+
+class PhotoUploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+    def post(self):
+        upload = self.get_uploads()[0]
+        user_photo = UserPhoto(
+            user="users.get_current_user().user_id()",
+            blob_key=upload.key())
+        user_photo.put()
+
+        self.redirect('/view_photo/%s' % upload.key())
+
+
+class ViewPhotoHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self, photo_key):
+        if not blobstore.get(photo_key):
+            self.error(404)
+        else:
+            self.send_blob(photo_key)
+
+
+app = webapp2.WSGIApplication([
+    ('/', PhotoUploadFormHandler),
+    ('/upload_photo', PhotoUploadHandler),
+    ('/view_photo/([^/]+)?', ViewPhotoHandler),
+], debug=True)
